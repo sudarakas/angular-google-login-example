9d4bf220e067897d7057e0d366e764b6bc823624
sudarakas <sudharakafb@gmail.com>
2022-10-01 19:45:51 +0530

    add SocialAuthServiceConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {GoogleLoginProvider} from 'angularx-social-login';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,18 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('***********************************.apps.googleusercontent.com') // set the Google client id
+        }
+      ]
+    }
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
