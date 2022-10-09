0669c742102c765663271b7c55a37cd56eef859f
Vishal <40030063+VishalTharindu@users.noreply.github.com>
2022-10-09 12:19:18 +0530

    include navigation items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,44 +5,43 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
-import {GoogleLoginProvider} from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthService } from './auth-service.service';
 import { NavbarComponent } from './navbar/navbar.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    NavbarComponent
-  ],
+  declarations: [AppComponent, LoginComponent, HomeComponent, NavbarComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path: 'login', component: LoginComponent},
-      {path: 'home', component: HomeComponent, canActivate: [AuthService]},
-      {path: '**', component: LoginComponent}
+      { path: 'login', component: LoginComponent },
+      { path: 'home', component: HomeComponent, canActivate: [AuthService] },
+      { path: '**', component: LoginComponent },
     ]),
     AppRoutingModule,
-    MatCardModule
+    MatCardModule,
   ],
-  providers: [{
-    provide: 'SocialAuthServiceConfig',
-    useValue: {
-      autoLogin: true,
-      providers: [
-        {
-          id: GoogleLoginProvider.PROVIDER_ID,
-          provider: new GoogleLoginProvider('***********************************.apps.googleusercontent.com') // set the Google client id
-        }
-      ]
-    }
-  }, AuthService],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: true,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '***********************************.apps.googleusercontent.com'
+            ), // set the Google client id
+          },
+        ],
+      },
+    },
+    AuthService,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
